b0ff716d2cda17952c53f6f65c4813c4421a7472
Maureenkamau03 <mkaymaureen@gmail.com>
2022-10-05 16:07:25 +0300

    fetching data

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import "../styles/Tasks.css";
 
+function Tasks() {
+	// Fetching data from the API
+	const [fetchedTasks, setFetchedTasks] = React.useState([]);
+
+	React.useEffect(() => {
+		getTasks();
+	}, []);
+	const getTasks = () => {
+		fetch("http://localhost:8000/tasks")
+			.then((res) => res.json())
+			.then((data) => {
+				setFetchedTasks(data);
+			});
+	};
+	console.log(fetchedTasks);
+	return <div></div>;
+}
+
+export default Tasks;
