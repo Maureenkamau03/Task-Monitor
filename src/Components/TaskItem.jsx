5c2479dc6d94fdd07fe9281d5d3bc97ab7bafb7a
Maureenkamau03 <mkaymaureen@gmail.com>
2022-10-06 18:25:10 +0300

    Marking task as done

--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -15,22 +15,25 @@ function TaskItem({
 		comments: "",
 		completed: false,
 	});
+	const [complete, setComplete] = React.useState(completed);
 	const inputChangeHandler = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 	const submitHandler = (e) => {
 		e.preventDefault();
 		//patch
+		const toSubmit = { ...formData, completed: true };
 		fetch("http://localhost:8000/tasks/" + id, {
 			method: "PATCH",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(formData),
+			body: JSON.stringify(toSubmit),
 		})
 			.then((response) => response.json())
 			.then((data) => {
 				console.log("Success:", data);
+				setComplete(true);
 			});
 
 		// const solutionData = {
@@ -51,26 +54,32 @@ function TaskItem({
 				{/* <h2>Student Name: {studentName}</h2> */}
 			</div>
 			<div className="desc">Description: {description}</div>
-			<form action="" onSubmit={submitHandler}>
-				<label htmlFor="solution">Solution </label>
-				<input
-					type="text"
-					name="solution"
-					value={formData.solution}
-					onChange={inputChangeHandler}
-					placeholder="Add a description of how you solved the probem"
-				/>
-				<label htmlFor="">Comment</label>
-				<input
-					type="text"
-					name="comments"
-					id=""
-					value={formData.comments}
-					onChange={inputChangeHandler}
-					placeholder="If task not done or challenges encountered leave a comment here"
-				/>
-				<input type="submit" value="Submit" />
-			</form>
+			{!complete ? (
+				<form action="" onSubmit={submitHandler}>
+					<label htmlFor="solution">Solution </label>
+					<input
+						type="text"
+						name="solution"
+						value={formData.solution}
+						onChange={inputChangeHandler}
+						placeholder="Add a description of how you solved the probem"
+					/>
+					<label htmlFor="">Comment</label>
+					<input
+						type="text"
+						name="comments"
+						id=""
+						value={formData.comments}
+						onChange={inputChangeHandler}
+						placeholder="If task not done or challenges encountered leave a comment here"
+					/>
+					<input type="submit" value="Submit" />
+				</form>
+			) : (
+				<div>
+					<p>Done</p>
+				</div>
+			)}
 		</div>
 	);
 }
