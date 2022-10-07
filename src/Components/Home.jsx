7986a8c5c47b88adcab1cae9131cc1b38871b516
Maureenkamau03 <mkaymaureen@gmail.com>
2022-10-07 07:11:13 +0300

    added footer component and styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Home.css";
+import Footer from "./Footer";
 
 function Home() {
 	return (
@@ -7,6 +8,7 @@ function Home() {
 			<div className="hero">
 				{" "}
 				<div className="herotext">
+					<h1>Monitoring tasks has never been this easy</h1>
 					<p>
 						Has it been tedious to physically track your juniours' tasks
 						manually?
@@ -23,6 +25,7 @@ function Home() {
 					alt=""
 				/>
 			</div>
+			<Footer />
 		</div>
 	);
 }
